9764f3784b29579ea6c56654b69b8ae82b77f9ae
Giorgia Marcantonio <giorgia.marcantonio@outlook.it>
2025-02-27 17:10:49 +0100

    sezione 2 conta numeri terminata con setInterval e IntersectionObserver

--- main.js
+++ main.js
@@ -7,3 +7,38 @@ window.addEventListener("scroll", ()=> {
         navbar.classList.remove("scrolledNav")
     }
 })
+
+// CREATE INTERVAL
+let productsNum = document.querySelector("#productsNum")
+let brandsNum = document.querySelector("#brandsNum")
+let reviewsNum = document.querySelector("#reviewsNum")
+
+function createInterval(finalNumber, element, speed){
+    let counter = 0
+    let interval = setInterval(()=> {
+        if(counter < finalNumber){
+            counter++
+            element.innerHTML = counter 
+        } else {
+            clearInterval(interval)
+        }
+    }, speed)
+}
+
+// INTERSECTION OBSERVER
+
+let isStarted = false
+
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach ((el)=> {
+        if(el.isIntersecting && isStarted == false){
+            createInterval(1000, productsNum, 6)
+            createInterval(200, brandsNum, 30)
+            createInterval(500, reviewsNum, 15)
+            isStarted = true
+        }
+    })
+})
+
+let elInterection = document.querySelector("#elInterection")
+observer.observe(elInterection)
