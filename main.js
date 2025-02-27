2ad8ccb1e6a114212eb5fa0af53135db4da4d42a
Giorgia Marcantonio <giorgia.marcantonio@outlook.it>
2025-02-27 18:05:11 +0100

    terminata sezione ultimi 3 articoli

--- main.js
+++ main.js
@@ -42,3 +42,36 @@ const observer = new IntersectionObserver((entries)=> {
 
 let elInterection = document.querySelector("#elInterection")
 observer.observe(elInterection)
+
+
+// ULTIMI ARTICOLI
+
+const products = [
+    {"name": "Lavabo marmo", "category": "Bagno", "prezzo": 560},
+    {"name": "Libreria", "category": "Living", "prezzo": 5809},
+    {"name": "Poltrona", "category": "Living", "prezzo": 780},
+    {"name": "Tavolo in marmo", "category": "Cucina", "prezzo": 2400},
+    {"name": "Letto fluttuante", "category": "Camera da Letto", "prezzo": 1550},
+    {"name": "Specchiera", "category": "Bagno", "prezzo": 990},
+]
+
+let cardWrapper = document.querySelector("#cardWrapper")
+
+products.forEach((product, i)=> {
+    if( i >= products.length -3){
+        let div = document.createElement("div")
+        div.classList.add("col-12", "col-md-4", "col-lg-3", "mx-lg-1", "my-1")
+        div.innerHTML = `
+                        <div class="card-custom">
+                        <img class="imgCardArt" src="https://picsum.photos/20${i}" alt="">
+                        <h5 class="cormorant text-center text-uppercase mt-5 fw-bold color-p">${product.name}</h5>
+                        <p class="color-t fw-semibold">Categoria: ${product.category}</p>
+                        <p class="color-t fw-semibold">Prezzo: <span class="color-t">${product.prezzo}$</span></p>
+                        <div class="d-flex justify-content-center">
+                            <button class="btn btnIndexCustom">Compra</button>
+                        </div>
+                        </div>
+                        `
+        cardWrapper.appendChild(div)
+    }
+})
